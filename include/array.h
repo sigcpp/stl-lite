813544aabb1c4e6ab485b261ebdef8c07b9183d3
Sean Murthy <murthys@wcsu.edu>
2020-03-15 12:15:43 -0400

    Add array template implementation
    Iterators yet to be implemented

---
+++ include/array.h
@@ -0,0 +1,84 @@
+/*
+* array.h
+* Sean Murthy 
+* (c) 2020 sigcpp https://sigcpp.github.io
+*
+* Define a class template for arrays
+* - see C++17 [array.overview] https://timsong-cpp.github.io/cppwp/n4659/array
+*/
+
+#ifndef SIGCPP_ARRAY_H
+#define SIGCPP_ARRAY_H
+
+#include <cstddef>
+#include <stdexcept>
+#include <utility>
+#include <type_traits>
+
+namespace sigcpp
+{
+
+	template<typename T, std::size_t N>
+	struct array
+	{
+		//types
+		using value_type = T;
+		using pointer = T*;
+		using const_pointer = const T*;
+		using reference = T&;
+		using const_reference = const T&;
+		using size_type = size_t;
+		using difference_type = ptrdiff_t;
+
+		//using iterator = implementation-defined;
+		//using const_iterator = implementation - defined;
+		//using reverse_iterator = std::reverse_iterator<iterator>;
+		//using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+
+		//underlying array
+		value_type elements[N];
+
+		//utility
+		void fill(const T& u) { for (auto& v : elements) v = u; }
+
+		void swap(array& a) noexcept(std::is_nothrow_swappable_v<T>)
+		{
+			for (size_type i = 0; i < N; i++)
+				std::swap(elements[i], a[i]);
+		}
+	
+		//capacity
+		constexpr bool empty() const noexcept { return N == 0; }
+		constexpr size_type size() const noexcept { return N; }
+		constexpr size_type max_size() const noexcept { return N; }
+
+		// element access
+		constexpr reference operator[](size_type pos) { return elements[pos]; }
+		constexpr const_reference operator[](size_type pos) const { return elements[pos]; }
+
+		constexpr reference at(size_type pos)
+		{
+			if (pos < N)
+				return elements[pos];
+			else
+				throw new std::out_of_range("array index out of range");
+		}
+
+		constexpr const_reference at(size_type pos) const { return at(pos); }
+
+
+		constexpr reference front() { return elements[0]; }
+		constexpr const_reference front() const { return elements[0]; }
+
+		constexpr reference back() { return elements[N-1]; }
+		constexpr const_reference back() const { return elements[N - 1]; }
+
+		constexpr pointer data() noexcept { return elements; }
+		constexpr const_pointer data() const noexcept { return elements;  }
+
+	}; //template array
+
+}	//namespace sigcpp
+
+#endif
