9c10765dd9b4f7d58163fb6a6f5bb748e801d9b8
Sean Murthy <murthys@wcsu.edu>
2020-03-17 18:40:00 -0400

    Test swap

--- test/main.cpp
+++ test/main.cpp
@@ -6,7 +6,6 @@
 * Test stl-lite
 */
 
-
 #include <iostream>
 
 #include "../include/array.h"
@@ -15,7 +14,8 @@ int main()
 {
     using sigcpp::array;
     
-    array<int, 10> i;
+    array<int, 10> i{ 8, 5, 4 };
+    array<int, 10> j{ 10, -2, 7 };
     array<float, 5> f;
     array<double, 25> d;
 
@@ -26,4 +26,13 @@ int main()
     constexpr array<double, 25>::size_type p = d.size();
 
     std::cout << m << ' ' << n << ' ' << p << '\n';
+
+    //test swap
+    std::cout << i[0] << ' ' << i[1] << ' ' << i[2] << '\n';
+    std::cout << j[0] << ' ' << j[1] << ' ' << j[2] << '\n';
+
+    i.swap(j);
+
+    std::cout << i[0] << ' ' << i[1] << ' ' << i[2] << '\n';
+    std::cout << j[0] << ' ' << j[1] << ' ' << j[2] << '\n';
 }
