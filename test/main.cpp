4008baba5c9b5790dbdd9fc1a53c2afa0891db2a
Ndemco <50844654+Ndemco@users.noreply.github.com>
2020-04-09 20:00:56 -0400

    Add support for -h -p and -s cmd-line options
    Also partial support for -ht and -f options.

--- test/main.cpp
+++ test/main.cpp
@@ -12,22 +12,38 @@
 */
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <array>
 #include <vector>
+#include <cstring>
+#include <string>
 #include <string_view>
 #include <cstddef>
+#include <sstream>
+#include <regex>
 
 #include "tester.h"
 
 //must be defined in a unit-specific source file such as "array-test.cpp"
 void runTests();
 
+bool fileExists(const char* fileName);
+
+void processArguments(std::vector<std::string_view> arguments,
+    bool& printHeader, bool& printSummary, std::ostream& out);
+
 int main(int argc, char* argv[])
 {
     std::vector<std::string_view> arguments;
     for (int i = 0; i < argc; ++i)
         arguments.emplace_back(argv[i]);
+
+    bool printHeader{ true };
+    bool printSummary{ true };
+    char header[512]{ "Running " };
+    strcat_s(header, argv[0]);
+    std::ostream& out = std::cout;
 
    try
    {
@@ -44,4 +60,79 @@ int main(int argc, char* argv[])
    summarizeTests();
 
    return getTestsFailed();
+}
+
+
+void processArguments(std::vector<std::string_view> arguments,
+    bool& printHeader, bool& printSummary, std::ostream& out)
+{
+    for (std::size_t i = 1; i < arguments.size(); i+=2)
+    {
+        if (arguments[i] == "-h")
+        {
+            if (arguments[i + 1] == "yes")
+                printHeader = true;
+            else if (arguments[i + 1] == "no")
+                printHeader = false;
+        }
+        
+        else if (arguments[i] == "-ht")
+        {
+            //process text
+        }
+
+        else if (arguments[i] == "-p")
+        {
+            if (arguments[i + 1] == "detail")
+                setPassReportMode(passReportMode::detail);
+            else if (arguments[i + 1] == "indicate")
+                setPassReportMode(passReportMode::indicate);
+            else if (arguments[i + 1] == "none")
+                setPassReportMode(passReportMode::none);
+            else if (arguments[i + 1] == "auto")
+            {
+                for (std::size_t i = 1; i < arguments.size(); i += 2)
+                {
+                    if (arguments[i] == "-f" || arguments[i] == "-fo"
+                        || arguments[i] == "-fa")
+                        setPassReportMode(passReportMode::none);
+                    else
+                        setPassReportMode(passReportMode::indicate);
+                }
+            }
+        }
+        else if (arguments[i] == "-s")
+        {
+            if (arguments[i + 1] == "yes")
+                printSummary = true;
+            else if (arguments[i + 1] == "no")
+                printSummary = false;
+        }
+        else if (arguments[i] == "-t")
+        {
+            //need to convert arguments[i + 1] from string_view to unsigned
+            //and supply that argument to setFailThreshold() function
+        }
+        else if (arguments[i] == "-f")
+        {
+            //convert filename to c-string
+            //check if filename has a file extension, if it doesn't, append .out
+            //test if file "arguments[i + 1]" exists, if it does print error message
+            //else construct an ofstream that writes to filename
+        }
+        else if (arguments[i] == "-fo")
+        {
+
+        }
+        else if (arguments[i] == "-fa")
+        {
+
+        }
+    }
+}
+
+bool fileExists(const char* fileName)
+{
+    struct stat buffer;
+    return (stat(fileName, &buffer) == true);
 }
