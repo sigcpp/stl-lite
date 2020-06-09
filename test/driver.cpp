1b16ff3c60636fc01170b42bb6a44eb95454ff1a
Sean Murthy <s.murthy@outlook.com>
2020-06-09 12:48:42 -0400

    Add intial exception handline

--- test/driver.cpp
+++ test/driver.cpp
@@ -14,19 +14,49 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <exception>
 
 #include "tester.h"
 #include "options.h"
+#include "options-exceptions.h"
 
 //must be defined in a unit-specific source file such as "array-test.cpp"
 void runTests();
 
+void show_error(const char* message);
+void show_usage();
+void show_error_and_usage(const char* message);
+
 int main(int argc, char* argv[])
 {
-	Options options = get_options(argv, argc);
-
+	Options options;
 	std::ofstream fileOut;
-	apply_options(options, fileOut);
+
+	try {
+		options = get_options(argv, argc);
+		apply_options(options, fileOut);
+	}
+	catch (const cmd_line_error& cle) {
+		show_error_and_usage(cle.what());
+		return -1;
+	}
+	catch (const std::exception& e) {
+		show_error(e.what());
+		return -2;
+	}
+	catch (const std::string& s) {
+		show_error(std::string("unexpected error: " + s).data());
+		return -3;
+	}
+	catch (const char* z) {
+		show_error((std::string("unexpected error: ") + z).data());
+		return -4;
+	}
+	catch (...) {
+		show_error("Unexpected and untyped error");
+		return -5;
+	}
+
 
 	try {
 		runTests();
@@ -39,4 +69,24 @@ int main(int argc, char* argv[])
 		summarizeTests();
 
 	return getTestsFailed();
+}
+
+
+void show_error_and_usage(const char* message)
+{
+	show_error(message);
+	std::cout << '\n';
+	show_usage();
+}
+
+
+void show_error(const char* message)
+{
+	std::cout << message << '\n';
+}
+
+
+void show_usage()
+{
+	std::cout << "array-test.exe -h yes/no -ht <header text>" << '\n';
 }
