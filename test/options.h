7483f75ebf0ba978fcce9a37fa3a572f54bcea76
Sean Murthy <s.murthy@outlook.com>
2020-06-12 17:02:21 -0400

    Change default header text to "Running $suite"

--- test/options.h
+++ test/options.h
@@ -24,7 +24,7 @@ enum class file_open_mode { no_file, new_file, overwrite, append };
 struct Options {
 	bool header{ true };
 	bool summary{ true };
-	std::string header_text{ "Running $cmd" };
+	std::string header_text{ "Running $suite" };
 	pass_report_mode prm{ pass_report_mode::indicate };
 	unsigned short fail_threshold = 0;
 	file_open_mode fom{ file_open_mode::no_file };
