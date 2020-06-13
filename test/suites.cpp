690bf42da8a6b8d75acd55d96b26aff6f53659a1
Sean Murthy <s.murthy@outlook.com>
2020-06-13 12:47:51 -0400

    "Simplify" test suite definition
    Define a set of macros to create a 1-step means of adding a test suite to suites collection. Made the definition process look declarative.

--- test/suites.cpp
+++ test/suites.cpp
@@ -8,7 +8,9 @@
 * - Copyright notice cannot be altered
 * Attribution and copyright info may be relocated but they must be conspicuous.
 *
-* Define functions to manage collection of test suites
+* Define a collection of test suites and functions to manage the collection
+* Edit this with a lot of care: many carefully designed macros to make suite definition easy and safe
+* Go to the section at the end of this file to add a test suite
 */
 
 #include <string>
@@ -16,32 +18,61 @@
 
 #include "suites.h"
 
-//all test suites defined
+//keyed collection of all test suites defined 
 static std::unordered_map<std::string, suite_runner_type> test_suites;
+
+//macros to facilitate 1-step definition and addition of suite runners to suites collection
+//TEST_SUITE is the only macro necessary, but the other macros are defined and used so that
+//the section where test suites are added looks cohesive
+
+#define DECLARE_BUILD_SUITES_COLLECTION void build_suites_collection();
+
+#define BUILD_SUITES_COLLECTION \
+	if (collection_built) \
+		build_suites_collection();
+
+#define START_SUITES_COLLECTION \
+void build_suites_collection() \
+{
+
+#define END_SUITES_COLLECTION \
+}
+
+
+//macro to declare a suite runner function and to add it to the suites collection
+//Example: TEST_SUITE(array_test) is to add the following two lines of code:
+// void array_test();
+// test_suites["array_test"] = array_test;
+#define TEST_SUITE(SUITE_NAME) \
+	void SUITE_NAME(); \
+	test_suites[#SUITE_NAME] = SUITE_NAME;
 
 //flag to denote if suites collection is already built
 static bool collection_built{ true };
-
-
-void build_suites_collection();
 
 //provide read-only access to collection of all test suites defined
 //build collection "just in time" if it has not already been built
 const std::unordered_map<std::string, suite_runner_type>& get_test_suites()
 {
-	if (collection_built)
-		build_suites_collection();
-	
+	DECLARE_BUILD_SUITES_COLLECTION; // void build_suites_collection();
+	BUILD_SUITES_COLLECTION;		 // if (collection_built) build_suites_collection();
+
 	collection_built = false;
 	return test_suites;
 }
 
 
-//this function is intentionally placed at the end of file to make it easier and safer to add test suites
-//the collection is as good as being built with a list initializer ctor, but the technique used here makes it
-//easier to add suites
-void build_suites_collection()
-{
-	void array_test();
-	test_suites["array_test"] = array_test;
-}
+//this section is intentionally placed at the end of file to make it easier and safer to add test suites
+//the collection is as good as being built with a initializer-list ctor, but the technique used here provides
+//a 1-step approach instead of the 2-step approach that would be necessary if initializer-list ctor is used
+//(2 steps: declare suite runner and add suite runner to the collection)
+
+START_SUITES_COLLECTION //same as void build_suites_collection() {  
+
+	//add one line per test suite: macro parameter should be the name of a suite runner function
+	//the semi-colon at the end of macro invocation is not required but its use make things look authentic
+
+	TEST_SUITE(array_test);
+
+// do not add/edit anything after this line
+END_SUITES_COLLECTION	// same as }
