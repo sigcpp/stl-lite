e383f8e156372ed142d3f310597d6e4ca4c9cc02
Ndemco <50844654+Ndemco@users.noreply.github.com>
2020-06-15 14:11:00 -0400

    Test get_options more comprehensively

--- test/driver-test.cpp
+++ test/driver-test.cpp
@@ -21,25 +21,58 @@
 void driver_test()
 {
 	//names in name-value pair
-	char path[]{ "C:/Libraries/stl-lite/array-test.exe" }, h[]{ "-h" }, ht[]{ "-ht" }, p[]{ "-p" },
+	char path[]{ "C:/Libraries/stl-lite/array_test.exe" }, h[]{ "-h" }, ht[]{ "-ht" }, p[]{ "-p" },
 		s[]{ "-s" }, t[]{ "-t" }, run[]{ "-run" }, fn[]{ "-fn" }, fo[]{ "-fo" }, fa[]{ "-fa" };
 
 	// values in name-value pair
 	char yes[]{ "yes" }, no[]{ "no" }, thresh[]{ "3" }, cmd[]{ "$cmd" }, indicate[]{ "indicate" },
-		detail[]{ "detail" }, none[]{ "none" }, array[]{ "array-test" };
+		detail[]{ "detail" }, none[]{ "none" }, command_name[]{ "array_test" };
 
-	
-	char* args[]{ path, h, yes, ht, cmd, p, detail, s, no, t, thresh, fn, cmd, run, array  };
-	std::size_t size = (sizeof(args) / sizeof(args[0]));
+	//array with only path to test the default behavior
+	char* default_args[]{ path };
+	std::size_t size = (sizeof(default_args) / sizeof(default_args[0]));
 
-	Options o = get_options(args, size);
+	Options o = get_options(default_args, size);
 
-	is_true(o.header == true, "-h yes");
+	is_true(o.header == true, "Default header = yes");
+	is_true(o.summary == true, "Default summary = yes");
+	is_true(o.header_text == "Running $suite", "Default header text");
+	is_true(o.prm == pass_report_mode::indicate, "Default PRM w/ no file output = indicate");
+	is_true(o.fail_threshold == 0, "Default fail threshold = 0");
+	is_true(o.command_name == command_name, "command_name");
+	is_true(o.fom == file_open_mode::no_file, "Default FOM = no_file");
+
+	//array with dummy arguments to test behavior of explicit options
+	char* args[]{ path, h, no, p, detail, s, no, t, thresh, fn, cmd, run, command_name  };
+	size = (sizeof(args) / sizeof(args[0]));
+
+	o = get_options(args, size);
+
+	is_true(o.header == false, "-h no");
 	is_true(o.summary == false, "-s no");
-	is_true(o.header_text == array, "-ht $cmd");
 	is_true(o.prm == pass_report_mode::detail, "-p detail");
 	is_true(o.fail_threshold == 3, "-t 3");
-	is_true(o.command_name == array, "command_name");
-	is_true(o.fom == file_open_mode::new_file && o.output_filepath == "array-test.out", "-fn $cmd");
-	is_true(o.suites_to_run == array, "-run array-test");
+	is_true(o.command_name == command_name, "command_name");
+	is_true(o.fom == file_open_mode::new_file && o.output_filepath == "array_test.out", "-fn $cmd");
+	is_true(o.suites_to_run == command_name, "-run array_test");
+
+	args[9] = fo;
+	args[4] = none;
+
+	o = get_options(args, size);
+	is_true(o.fom == file_open_mode::overwrite && o.output_filepath == "array_test.out", "-fo $cmd");
+	is_true(o.prm == pass_report_mode::none, "-p none");
+
+	args[9] = fa;
+	args[4] = indicate;
+	o = get_options(args, size);
+	is_true(o.fom == file_open_mode::append && o.output_filepath == "array_test.out", "-fa $cmd");
+	is_true(o.prm == pass_report_mode::indicate, "-p indicate");
+
+	args[2] = yes;
+	args[3] = ht;
+	args[4] = cmd;
+	o = get_options(args, size);
+	is_true(o.header == true, "-h yes");
+	is_true(o.header_text == command_name, "-ht $cmd");
 }
